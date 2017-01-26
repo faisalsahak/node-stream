61f63c20c55e3637ebad0d6c9acae87b2e35b69f
faisalsahak <sahak.faisal@gmail.com>
2017-01-25 16:48:58 -0800

    added side nav, link to the live tv, created the appropriate pages to the side link just first one

--- app.js
+++ app.js
@@ -8,6 +8,25 @@ app.get('/', function (req, res){
   res.render('index.ejs')
 });
 
+// app.get('/nfl', function (req, res){
+//   res.render('Pages/sportPages/nfl/nfl.ejs')
+// });
+// app.get('/game', function (req, res){
+//   res.render('Pages/sportPages/nfl/stream.ejs')
+// });
+
+app.get('/sky1', function (req, res){
+  res.render('Pages/sportChannels/skySports/skysport1.ejs')
+});
+app.get('/sky1Channel1', function (req, res){
+  res.render('Pages/sportChannels/skySports/sky1Channel1.ejs')
+});
+app.get('/sky1Channel2', function (req, res){
+  res.render('Pages/sportChannels/skySports/sky1Channel2.ejs')
+});
+app.get('/sky1Channel3', function (req, res){
+  res.render('Pages/sportChannels/skySports/sky1Channel3.ejs')
+});
 
 app.listen(3000, function(){
   console.log("server running on port 3000")
